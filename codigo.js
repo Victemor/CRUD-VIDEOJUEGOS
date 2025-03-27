b0fd9fe840be261c4280c250becf56ca2de337b0
Victemor <145466901+Victemor@users.noreply.github.com>
2025-03-26 21:39:19 -0500

    primer commit

---
+++ codigo.js
@@ -0,0 +1,24 @@
+function adicionar(){
+let nombreVideojuego = "";
+let precioVideojuego = "";
+let plataformaVideojuego = "";
+let categoriaVideojuego = "";
+let editorVideojuego = "";
+let nombreVideojuegob = "";
+let usuario = "";
+let contraseña = "";
+let usuarioa = "";
+let contraseñaa = "";
+
+nombreVideojuego = document.getElementById("NombreVideojuego").value ;
+nombreVideojuego = document.getElementById("PrecioVideojuego").value ;
+nombreVideojuego = document.getElementById("PlataformaVideojuego").value ;
+nombreVideojuego = document.getElementById("CategoriaVideojuego").value ;
+nombreVideojuego = document.getElementById("EditorVideojuego").value ;
+nombreVideojuego = document.getElementById("NombreVideojuegob").value ;
+nombreVideojuego = document.getElementById("Usuario").value ;
+nombreVideojuego = document.getElementById("Contraseña").value ;
+nombreVideojuego = document.getElementById("Usuarioa").value ;
+nombreVideojuego = document.getElementById("Contraseñaa").value ;
+
+}
